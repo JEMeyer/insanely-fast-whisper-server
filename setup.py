722e7f3019ce6496064d2970038f511c71f316da
Joe Meyer <joseph@meyer.id>
2025-02-23 17:10:56 -0600

    i apparently need python-multipart

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ setup(
     name="whisper-server",
     version="1.0",
     packages=find_packages(),
-    install_requires=["transformers", "optimum", "accelerate", "fastapi", "uvicorn"],
+    install_requires=[
+        "transformers",
+        "optimum",
+        "accelerate",
+        "fastapi",
+        "uvicorn",
+        "python-multipart",
+    ],
 )
